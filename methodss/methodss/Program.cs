f3a0b88880ab973c6b0c1e9f2b04e67364626894
H Kübra Karagülle <kbrakrgll@gmail.com>
2022-01-24 17:22:05 +0300

    SubString keşfi
    geleceği yazanlar ilk ödev

---
+++ methodss/methodss/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+
+public class methodss
+{
+    public static void Main()
+    {
+
+        string b = " deger girin";
+        Console.WriteLine(b.Substring(1, 9));
+
+        string a = Console.ReadLine();
+        Console.WriteLine(a.Substring(1, 7)); 
+        
+        //kullanıcıdan aldığım değerin ilk indisinden başlayıp, sonraki 6 indisi bastırıyor.
+        
+    }
+}
